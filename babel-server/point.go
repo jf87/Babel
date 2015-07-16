8a71450c1f5ecc42b578c8cbdbebbe4960349de4
Jonathan Fuerst <jf@jofu.org>
2015-07-16 16:26:28 -0700

    forgot points.go ;-)

---
+++ babel-server/point.go
@@ -0,0 +1,32 @@
+package main
+
+type Point struct {
+	Desc  string   `json:"desc"`
+	Name  string   `json:"name"`
+	Objs  []Object `json:"objs"`
+	Props []Prop2  `json:"props,"`
+}
+
+type Object struct {
+	Data_type int    `json:"data_type"`
+	Desc      string `json:"desc"`
+	Name      string `json:"name"`
+	Props     []Prop `json:"props"`
+	Unit      int    `json:"unit"`
+}
+
+type Prop struct {
+	Instance int    `json:"instance"`
+	Type     int    `json:"type"`
+	Type_str string `json:"type_str"`
+}
+
+type Prop2 struct {
+	Adr       []int `json:"adr"`
+	Device_id int   `json:"device_id"`
+	Mac       []int `json:"mac"`
+	Max_apdu  int   `json:"max_apdu"`
+	Net       int   `json:"net"`
+}
+
+type Points []Point
